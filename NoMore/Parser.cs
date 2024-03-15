63f860d78d991bce1439d820f493e01d08b5f592
Tom Mason <wheybags@wheybags.com>
2024-03-15 22:41:31 +0100

    document parser a bit

--- NoMore/Parser.cs
+++ NoMore/Parser.cs
@@ -1,14 +1,49 @@
-﻿// Root                 = ObjectBody $End
-// ObjectBody           = Item ObjectBody | Nil
-// Item                 = $String ItemRest | "{" ObjectBody "}"
-// ItemRest             = Operator AfterOperator | Nil
-// Operator             = "=" | ">=" | "?=" | "<=" | "==" | "<" | ">"
-// AfterOperator        = $String MaybeObject | "{" ObjectBody "}"
-// MaybeObject          = "{" ObjectBody "}" | Nil
+﻿
+// See https://pdx.tools/blog/a-tour-of-pds-clausewitz-syntax
+
+// What is not supported?
+// 1: operators as key names: =="bar"
+// here '=' "should" be the key and 'bar' the value
+//
+// 2: objects with implicit operator: foo{bar=qux}
+// this should be equivalent to foo={bar=qux}
+//
+// 3: this [[]] syntax is not supported at all (I think it's not in ck3 though)
+// generate_advisor = {
+//   [[scaled_skill]
+//   $scaled_skill$
+//       ]
+//   [[!skill] if = {} ]
+// }
+//
+// 4: Missing / extra closing braces are not supported
+// Apparently they "should" be
+//
+// 5: semicolons
+// Apparently you can put semicolons in certain places and they should be ignored. This isn't supported
+//
+// 6: this "list" syntax:
+// simple_cross_flag = {
+//     pattern = list "christian_emblems_list"
+//     color1 = list "normal_colors"
+// }
+// because I haven't seen a real example, and I can't tell from this one how it should work
 
 
 public class Parser
 {
+    // This is a simple handwritten recursive descent parser
+    //
+    // Grammar:
+    // Root                 = ObjectBody $End
+    // ObjectBody           = Item ObjectBody | Nil
+    // Item                 = $String ItemRest | "{" ObjectBody "}"
+    // ItemRest             = Operator AfterOperator | Nil
+    // Operator             = "=" | ">=" | "?=" | "<=" | "==" | "<" | ">"
+    // AfterOperator        = $String MaybeObject | "{" ObjectBody "}"
+    // MaybeObject          = "{" ObjectBody "}" | Nil
+
+
     public static CkObject parse(string inputString)
     {
         Parser parser = new Parser();
